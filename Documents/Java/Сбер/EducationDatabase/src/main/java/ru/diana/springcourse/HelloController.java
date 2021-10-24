02d87d32b5de7bb8ac1c54744a3c45f1219825db
dianatoradze <dianatoradze1706@gmail.com>
2021-10-24 23:26:23 +0500

    example spring

---
+++ Documents/Java/Сбер/EducationDatabase/src/main/java/ru/diana/springcourse/HelloController.java
@@ -0,0 +1,14 @@
+//package ru.diana.springcourse;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//
+//@Controller
+//public class HelloController {
+//    @GetMapping("/hello-world")
+//    public String sayHello() {
+//        return "hello_world";
+//    }
+//}
